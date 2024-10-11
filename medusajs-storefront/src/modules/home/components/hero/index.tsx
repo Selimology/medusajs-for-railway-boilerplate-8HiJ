8211f78ffa8330f8547b850a1abb8e2e0c00ec44
Selimology <kertekin2018@gmail.com>
2024-10-11 14:58:47 +0200

    git commit -m "Revert changes in medusajs-storefront to commit eda12fd"

--- medusajs-storefront/src/modules/home/components/hero/index.tsx
+++ medusajs-storefront/src/modules/home/components/hero/index.tsx
@@ -42,14 +42,14 @@ const Hero = ({ heroSlides }: HeroProp) => {
                       Shop Now
                     </LocalizedClientLink>
                   </div>
-                  <div className={`sub-img absolute left-[300px] top-0 w-full h-full z-[-1]`}>
+                  <div className={`sub-img absolute ${slide.imageClasses} z-0`}>
                     <Image
                       src={slide.imageSrc}
                       width={slide.imageWidth}
                       height={slide.imageHeight}
                       alt={slide.imageAlt}
                       priority={true}
-                      className="w-full h-full object-cover"
+                      className="w-full"
                     />
                   </div>
                 </div>
